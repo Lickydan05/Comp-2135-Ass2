105d5ae3e4c74ee438fbc5966d344008839470c4
nceeoh7 <51413645+nceeoh7@users.noreply.github.com>
2023-04-04 16:15:54 +0000

    Profile implemented on ejs

--- index.js
+++ index.js
@@ -1,13 +1,23 @@
 const express = require("express");
 const ejs = require("ejs");
 const path = require("path");
+const bodyParser = require("body-parser");
+const expressSession = require("express-session")
+
 const app = express();
 
 //Sets our view engine to load files ending in .ejs
 app.set("view engine", "ejs");
 app.use(express.static("public"));
 
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
 const PORT = 5000;
+
+app.use(expressSession({
+  secret:"secret key"
+}))
 
 app.listen(PORT, () => {
   console.log("App listening on port ", PORT);
@@ -19,7 +29,10 @@ app.get("/", (req, res) => {
 });
 
 app.get("/profile", (req, res) => {
-  res.sendFile(path.resolve(__dirname + "/views/", "profile.html"));
+  console.log(req.session)
+
+  let user = req.session.user;
+  res.render("profile", {user})
 });
 
 app.get("/math", (req, res) => {
@@ -37,3 +50,11 @@ app.get("/terms", (req, res) => {
 app.get("/slides", (req, res) => {
   res.sendFile(path.resolve(__dirname + "/views/", "slide-show.html"));
 });
+
+app.post("/update-profile", (req, res)=>{
+  console.log(req.body);
+
+  req.session.user= req.body;
+
+  res.redirect("/profile")
+})
